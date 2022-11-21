a1ee55486d144650a46f5738a9a65a214785e44e
Siyuan <suquark@gmail.com>
2022-11-20 23:14:17 -0800

    update

--- src/evaluater/predict.py
+++ src/evaluater/predict.py
@@ -8,30 +8,12 @@ from handlers.model_builder import Nima
 from handlers.data_generator import TestDataGenerator
 
 
-def image_file_to_json(img_path):
-    img_dir = os.path.dirname(img_path)
-    img_id = os.path.basename(img_path).split('.')[0]
-
-    return img_dir, [{'image_id': img_id}]
-
-
-def image_dir_to_json(img_dir, img_type='jpg'):
-    img_paths = glob.glob(os.path.join(img_dir, '*.'+img_type))
-
-    samples = []
-    for img_path in img_paths:
-        img_id = os.path.basename(img_path).split('.')[0]
-        samples.append({'image_id': img_id})
-
-    return samples
-
-
 def predict(model, data_generator):
     return model.predict_generator(data_generator, workers=8, use_multiprocessing=True, verbose=1)
 
 
-def main(base_model_name, weights_file, image_list, predictions_file, img_format='jpg'):
-    samples = [{'image_id': img_path} for img_path in image_list.split(":")]
+def main(base_model_name, weights_file):
+    samples = [{'image_id': f"/images/{n}"} for n in os.listdir("/images")]
 
     # build model and load weights
     nima = Nima(base_model_name, weights=None)
@@ -48,10 +30,7 @@ def main(base_model_name, weights_file, image_list, predictions_file, img_format
     for i, sample in enumerate(samples):
         sample['mean_score_prediction'] = calc_mean_score(predictions[i])
 
-    print(json.dumps(samples, indent=2))
-
-    if predictions_file is not None:
-        save_json(samples, predictions_file)
+    save_json(samples, "/output/results.json")
 
 
 if __name__ == '__main__':
@@ -59,7 +38,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--base-model-name', help='CNN base model name', required=True)
     parser.add_argument('-w', '--weights-file', help='path of weights file', required=True)
-    parser.add_argument('-is', '--image-list', help='list of images seperated by colon', required=True)
     parser.add_argument('-pf', '--predictions-file', help='file with predictions', required=False, default=None)
 
     args = parser.parse_args()
